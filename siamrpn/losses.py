446dda761c56f71a680f40a84725792850551093
Yuol96 <huangyc96@gmail.com>
2018-10-15 22:09:19 +0800

    try to rewrite MyLoss module

--- siamrpn/losses.py
+++ siamrpn/losses.py
@@ -49,3 +49,33 @@ class Myloss(Module):
         
         loss = torch.add(closs, lmbda, rloss)
         return loss
+
+# class Myloss(Module):
+#     def __init__(self):
+#         super (Myloss, self).__init__()
+#         return 
+    
+#     def forward(self, coutput, clabel, routput, rlabel, lmbda):
+#         """
+#         Input:
+#             - coutput: (bs, 5, 2, 17, 17)
+#             - clabel: (bs, 5, 17, 17)
+#             - routput: (bs, 20, 17, 17)
+#             - rlabel: (bs, 20, 17, 17)
+#             - lmbda: scalar
+#             Typically, bs==1
+#         """
+#         closs = F.cross_entropy(coutput.permute(0,2,1,3,4), clabel)
+
+# #        rloss = F.smooth_l1_loss(routput, rlabel)
+#         rloss = F.smooth_l1_loss(routput, rlabel, size_average=False, reduce=False)  # (bs, 20, 17, 17)
+        
+#         eq = (clabel==1).unsqueeze(2).float()  # (bs, 5, 1, 17, 17)
+#         rloss = rloss.view(-1,5,4,17,17).mul(eq).sum()  # scalar
+#         rloss = torch.div(rloss, eq.nonzero().shape[0]*4+1e-4)  # average over all non-zero rloss term
+
+#         loss = torch.add(closs, lmbda, rloss)
+
+#         return loss, closs, rloss
+
+
