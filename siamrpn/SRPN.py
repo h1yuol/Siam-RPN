41b2eb0315490d399162ee648c61f6df185e94db
Yuol96 <huangyc96@gmail.com>
2018-11-03 23:19:40 +0800

    apply kaiming_normal to conv1-4

--- siamrpn/SRPN.py
+++ siamrpn/SRPN.py
@@ -34,12 +34,14 @@ class SiameseRPN(nn.Module):
                 if bias:
                     m.bias.data.zero_()
 
-        normal_init(self.conv1, 0, 0.001, False)
-        normal_init(self.conv2, 0, 0.001, False)
-        normal_init(self.conv3, 0, 0.001, False)
-        normal_init(self.conv4, 0, 0.001, False)
+        # normal_init(self.conv1, 0, 0.001, False)
+        # normal_init(self.conv2, 0, 0.001, False)
+        # normal_init(self.conv3, 0, 0.001, False)
+        # normal_init(self.conv4, 0, 0.001, False)
         # normal_init(self.cconv, 0, 0.001, False, False)
         # normal_init(self.rconv, 0, 0.001, False, False)
+        for m in [self.conv1, self.conv2, self.conv3, self.conv4]:
+            nn.init.kaiming_normal_(m.weight, mode='fan_out')
 
         if self.cbam:
             for m in self.cbamLayer.modules():
